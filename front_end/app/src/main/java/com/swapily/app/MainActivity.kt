4ba7f8d73d9b35f402c7ff6a91eea46f838cd8f0
jihane diouri <jihane.diouri@etu.uae.ac.ma>
2026-05-20 17:43:34 +0100

    chi haja

--- front_end/app/src/main/java/com/swapily/app/MainActivity.kt
+++ front_end/app/src/main/java/com/swapily/app/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.swapily.app.ui.screens.LoginScreen
 
 class MainActivity : ComponentActivity() {
 
@@ -17,8 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            SwapilyApp()
-        }
+            LoginScreen()
+            }
     }
 }
 
